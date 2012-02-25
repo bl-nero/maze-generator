1f7128dd5d2e3e8d42a78c3e0deb66cec192a86e
Bartosz Leper <bleper@google.com>
2012-02-25 11:10:13 +0100

    Board kinda complete

--- src/board.go
+++ src/board.go
@@ -8,27 +8,49 @@ const (
 	E
 	S
 	W
-	maxDirection  = W
-	directionMask = maxDirection<<1 - 1
+	visitedBit    Field = 1 << iota
+	maxDirection        = W
+	directionMask uint8 = uint8(maxDirection)<<1 - 1
 )
 
-type state uint8
+var dirNames map[Direction]string = map[Direction]string{N: "N", E: "E", S: "S", W: "W"}
 
-const (
-	normal state = state(maxDirection) << (1 + iota)
-	visited
-)
+func (self Direction) String() string {
+	if self == None {
+		return "None"
+	}
+	res := ""
+	for dir, name := range dirNames {
+		if self&dir != 0 {
+			res += name
+		}
+	}
+	return res
+}
 
 type Field uint8
 
+func (f Field) Direction() Direction {
+	return Direction(f & Field(directionMask))
+}
+
+func (f Field) isVisited() bool {
+	return f&visitedBit != 0
+}
+
+type Point struct{ X, Y int }
 type Board struct {
-	fields [][]Direction
+	Fields         [][]Field
+	Entrance, Exit Point
 }
 
 func New(width, height int) Board {
-	board := Board{make([][]Direction, height)}
-	for i := range board.fields {
-		board.fields[i] = make([]Direction, width)
+	board := Board{Fields: make([][]Field, height)}
+	for i := range board.Fields {
+		board.Fields[i] = make([]Field, width)
 	}
 	return board
+}
+
+func (self *Board) walk() {
 }
--- src/board_test.go
+++ src/board_test.go
@@ -7,18 +7,46 @@ import (
 	"testing"
 )
 
-func TestCreatingBoard(t *testing.T) {
-	const width, height = 4, 5
-	board := New(width, height)
-	if board == nil {
-		t.Errorf("New board is nil")
-	}
-	if len(board) != 5 {
-		t.Errorf("Length of board is %d, expected 5", len(board))
-	}
-	for i, row := range board {
-		if len(row) != 4 {
-			t.Errorf("Length of row %d is %d, expected 4", i, len(row))
+func TestDirectionNames(t *testing.T) {
+	testCases := map[Direction]string{
+		None:          "None",
+		S:             "S",
+		N | W:         "NW",
+		N | E | S | W: "NESW"}
+	for dir, name := range testCases {
+		if dir.String() != name {
+			t.Errorf("Name of direction %d is %q, expected %q",
+				uint8(dir), dir.String(), name)
 		}
 	}
+}
+
+func TestCreatingBoard(t *testing.T) {
+	const width, height = 3, 2
+	board := New(width, height)
+	if len(board.Fields) != height {
+		t.Fatalf("Length of board is %d, expected %d", len(board.Fields), height)
+	}
+	for i, row := range board.Fields {
+		if len(row) != width {
+			t.Errorf("Length of row %d is %d, expected %d", i, len(row), width)
+		}
+		for j, field := range row {
+			if field.Direction() != None {
+				t.Errorf("Direction of (%d, %d) is %v", i, j,
+					field.Direction())
+			}
+			if field.isVisited() {
+				t.Errorf("Field (%d, %d) is visited")
+			}
+		}
+	}
+}
+
+type walkingTest struct {
+
+}
+
+func TestWalking(t *testing.T) {
+	//board := New(
 }
