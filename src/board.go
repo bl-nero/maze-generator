ba2e82fd2f7b2408b3cc423ce462df8370b6c031
Bartosz Leper <bleper@google.com>
2012-02-26 23:44:01 +0100

    Board becoming more and more mature

--- src/board.go
+++ src/board.go
@@ -1,5 +1,7 @@
 package board
 
+import "image"
+
 type Direction uint8
 
 const (
@@ -28,29 +30,93 @@ func (self Direction) String() string {
 	return res
 }
 
+var dirDeltas map[Direction]image.Point = map[Direction]image.Point{
+	N: {0, -1},
+	E: {1, 0},
+	S: {0, 1},
+	W: {-1, 0},
+}
+
+func (self Direction) Delta() (delta image.Point, ok bool) {
+	delta, ok = dirDeltas[self]
+	return
+}
+
 type Field uint8
 
 func (f Field) Direction() Direction {
 	return Direction(f & Field(directionMask))
 }
 
-func (f Field) isVisited() bool {
+func (f *Field) SetDirection(dir Direction) {
+	*f = Field(uint8(*f)&^directionMask | uint8(dir))
+}
+
+func (f *Field) AddDirection(dir Direction) {
+	*f |= Field(dir)
+}
+
+func (f Field) visited() bool {
 	return f&visitedBit != 0
 }
 
-type Point struct{ X, Y int }
-type Board struct {
-	Fields         [][]Field
-	Entrance, Exit Point
+func (f *Field) setVisited(visited bool) {
+	if visited {
+		*f = Field(visitedBit)
+	} else {
+		*f = Field(0)
+	}
+}
+
+type Board interface {
+	Width() int
+	Height() int
+	At(x, y int) *Field
+	Entrance() *image.Point
+	Exit() *image.Point
+	Walk() [][]bool
 }
 
 func New(width, height int) Board {
-	board := Board{Fields: make([][]Field, height)}
-	for i := range board.Fields {
-		board.Fields[i] = make([]Field, width)
+	board := boardImpl{fields: make([][]Field, height)}
+	for i := range board.fields {
+		board.fields[i] = make([]Field, width)
 	}
-	return board
+	return &board
 }
 
-func (self *Board) walk() {
+type boardImpl struct {
+	fields         [][]Field
+	entrance, exit image.Point
+}
+
+func (self *boardImpl) Width() int             { return len(self.fields[0]) }
+func (self *boardImpl) Height() int            { return len(self.fields) }
+func (self *boardImpl) At(x, y int) *Field     { return &self.fields[y][x] }
+func (self *boardImpl) Entrance() *image.Point { return &self.entrance }
+func (self *boardImpl) Exit() *image.Point     { return &self.exit }
+
+func (self *boardImpl) Walk() [][]bool {
+	visitMatrix := make([][]bool, self.Height())
+	for i := range visitMatrix {
+		visitMatrix[i] = make([]bool, self.Width())
+	}
+
+	p := *self.Entrance();
+	for !p.Eq(*self.Exit()) {
+		visitMatrix[p.Y][p.X] = true
+		delta, ok := self.At(p.X, p.Y).Direction().Delta()
+		if (!ok) {
+			return nil
+		}
+		p = p.Add(delta)
+	}
+	visitMatrix[p.Y][p.X] = true
+
+	/*for y, row := range visitMatrix {
+		for x := range row {
+			row[x] = self.At(x, y).Direction() != None
+		}
+	}*/
+	return visitMatrix
 }
--- src/board_test.go
+++ src/board_test.go
@@ -5,6 +5,7 @@ package board
 
 import (
 	"testing"
+	"image"
 )
 
 func TestDirectionNames(t *testing.T) {
@@ -21,32 +22,153 @@ func TestDirectionNames(t *testing.T) {
 	}
 }
 
+func performFieldValueTests(t *testing.T, f *Field, visited bool) {
+	f.setVisited(visited)
+	f.SetDirection(N)
+	if f.Direction() != N {
+		t.Errorf("Direction set to %v, expected N", f.Direction())
+	}
+	f.SetDirection(S)
+	if f.Direction() != S {
+		t.Errorf("Direction set to %v, expected S", f.Direction())
+	}
+	f.AddDirection(W)
+	if f.Direction() != S|W {
+		t.Errorf("Direction set to %v, expected %v", f.Direction(), S|W)
+	}
+	if f.visited() != visited {
+		t.Errorf("Visited flag is %v, expected %v", f.visited(), visited)
+	}
+	if f.Direction() != S|W {
+		t.Errorf("Direction after changing visited flag is %v, expected %v",
+			f.Direction(), S|W)
+	}
+}
+
+func TestFieldValues(t *testing.T) {
+	var f Field
+	if f.Direction() != None {
+		t.Errorf("Initial direction is %v, expected None", f.Direction())
+	}
+	if f.visited() {
+		t.Errorf("Field is initially visited")
+	}
+	performFieldValueTests(t, &f, false)
+	performFieldValueTests(t, &f, true)
+}
+
 func TestCreatingBoard(t *testing.T) {
 	const width, height = 3, 2
 	board := New(width, height)
-	if len(board.Fields) != height {
-		t.Fatalf("Length of board is %d, expected %d", len(board.Fields), height)
-	}
-	for i, row := range board.Fields {
-		if len(row) != width {
-			t.Errorf("Length of row %d is %d, expected %d", i, len(row), width)
-		}
-		for j, field := range row {
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			field := board.At(x, y)
 			if field.Direction() != None {
-				t.Errorf("Direction of (%d, %d) is %v", i, j,
+				t.Errorf("Direction of (%d, %d) is %v", x, y,
 					field.Direction())
 			}
-			if field.isVisited() {
-				t.Errorf("Field (%d, %d) is visited")
+			if field.visited() {
+				t.Errorf("Field (%d, %d) is visited", x, y)
 			}
 		}
 	}
 }
 
 type walkingTest struct {
+	Board       boardImpl
+	VisitMatrix [][]bool
+}
 
+var walkingTests []walkingTest = []walkingTest{
+
+	// + +
+	// | |
+	// + +
+	{
+		Board: boardImpl{
+			fields:   [][]Field{{Field(N | S)}},
+			entrance: image.Pt(0, 0),
+			exit:     image.Pt(0, 0),
+		},
+		VisitMatrix: [][]bool{{true}},
+	},
+
+	// +-+-+
+	// |   |
+	// + + +
+	// | | |
+	// + + +
+	{
+		Board: boardImpl{
+			fields: [][]Field{
+				{Field(E | S), Field(W | S)},
+				{Field(N | S), Field(N | S)},
+			},
+			entrance: image.Pt(0, 1),
+			exit:     image.Pt(1, 1),
+		},
+		VisitMatrix: [][]bool{{true, true}, {true, true}},
+	},
+
+	// +-+-+
+	//     |
+	// +-+ +
+	// |X|
+	// +-+-+
+	{
+		Board: boardImpl{
+			fields: [][]Field{
+				{Field(E | W), Field(W | S)},
+				{Field(None), Field(N | E)},
+			},
+			entrance: image.Pt(0, 0),
+			exit:     image.Pt(1, 1),
+		},
+		VisitMatrix: [][]bool{{true, true}, {false, true}},
+	},
+
+	// +-+-+
+	//
+	// +-+-+
+	// |X X|
+	// +-+-+
+	{
+		Board: boardImpl{
+			fields: [][]Field{
+				{Field(E | W), Field(E | W)},
+				{Field(E), Field(W)},
+			},
+			entrance: image.Pt(0, 0),
+			exit:     image.Pt(1, 0),
+		},
+		VisitMatrix: [][]bool{{true, true}, {false, false}},
+	},
 }
 
 func TestWalking(t *testing.T) {
-	//board := New(
+	for i, test := range walkingTests {
+		visitMatrix := test.Board.Walk()
+		if !matricesEqual(visitMatrix, test.VisitMatrix) {
+			t.Errorf("Visit matrix for test %d is %v, expected %v",
+				i, visitMatrix, test.VisitMatrix)
+		}
+	}
+}
+
+func matricesEqual(m1, m2 [][]bool) bool {
+	if len(m1) != len(m2) {
+		return false
+	}
+	for i, row1 := range m1 {
+		row2 := m2[i]
+		if len(row1) != len(row2) {
+			return false
+		}
+		for j, item1 := range row1 {
+			if item1 != row2[j] {
+				return false
+			}
+		}
+	}
+	return true
 }
